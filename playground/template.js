95776c30e6a8f1d347e701eb277cc1956a1edccb
Daniel Lewis <daniel.robert.lewis@gmail.com>
2025-11-08 21:29:41 -0800

    cleaner

--- playground/template.js
+++ playground/template.js
@@ -5,15 +5,4 @@ class PlaygroundSimulation extends GridSimulation {
         EMPTY: new Float32Array([0, 0, 0, 0])
     };
 
-    // Optional: Override randomize for custom behavior
-    randomize(probability = 0.3) {
-        const buffer = this.getCurrentBuffer();
-        const { ALIVE, EMPTY } = this.constructor.CellType;
-        for (let cellIdx = 0; cellIdx < this.width * this.height; cellIdx++) {
-            const cellType = Math.random() < probability ? ALIVE : EMPTY;
-            const bufferIdx = cellIdx * 4;
-            buffer.set(cellType, bufferIdx);
-        }
-        this.syncBuffer(buffer);
-    }
 }
